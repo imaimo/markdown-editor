83ae2acc801bc3cf6ecdf4ead55bd9ee9581aa7b
imaimo <kawamura.gaako@gmail.com>
2022-08-22 20:58:28 +0900

    webpackを導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+// import * as React from 'react'
+// import { render } from 'react-dom'
+
+// const Main = (<h1>Markdown Editor</h1>)
+
+// render(Main, document.getElementById('app'))
+
 import * as React from 'react'
-import { render } from 'react-dom'
-
-const Main = (<h1>Markdown Editor</h1>)
-
-render(Main, document.getElementById('app'))
+import("react-dom").then(render => {
+    const Main = (): JSX.Element => {
+        return <h1>Markdown Editor</h1>
+    }
+    render(Main, document.getElementById('app'))
+})
